2dd8523f9b777ad70c40769071bdb34f13336292
James Harrison <james.harrison.za@gmail.com>
2019-05-31 15:55:36 +0200

    A simple landing page. Using Bootstrap 4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Login extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	onClick() {
+
+		console.log(42)
+	}
+
+	render() {
+		
+		return (
+			<Container>
+				<Row className="justify-content-md-center">
+					<h1>Welcome</h1>
+				</Row>
+				<Row className="justify-content-md-center">
+					<div>
+						<p>Filter your saved songs for a given mood you are going for</p>
+					</div>
+				</Row>
+				<Row className="justify-content-md-center">
+					<Button
+						variant="outline-success"
+						onClick={() => this.onClick()}
+						>
+							Login to Spotify
+					</Button>
+				</Row>
+			</Container>
+		);
+	}
+}
+
+// ========================================
+
+ReactDOM.render(
+	<Login />,
+	document.getElementById('root')
+);
