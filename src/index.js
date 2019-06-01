c9fd9f89bd759f345b66b4169a594f0c132fc09c
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-06-01 11:40:05 +0200

    Getting access token !!!!!

--- src/index.js
+++ src/index.js
@@ -3,32 +3,37 @@ import ReactDOM from 'react-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Spotify from 'spotify-web-api-node';
+//import SpotifyWebApi from 'spotify-web-api-node';
+import queryString from 'query-string';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Login extends React.Component {
 
 	onClick() {
+		window.location = 'http://localhost:42420/login';
+	}
 
-		//Make server request
+	componentDidMount(){
+		const parsed = queryString.parse(window.location.search);
+		const accessToken = parsed.access_token;
+		console.log(`accessToken: ${accessToken}`);
 	}
 
 	render() {
-		
 		return (
 			<Container>
-				<Row className="justify-content-md-center">
+				<Row className='justify-content-md-center'>
 					<h1>Welcome</h1>
 				</Row>
-				<Row className="justify-content-md-center">
+				<Row className='justify-content-md-center'>
 					<div>
 						<p>Filter your saved songs for a given mood you are going for</p>
 					</div>
 				</Row>
-				<Row className="justify-content-md-center">
+				<Row className='justify-content-md-center'>
 					<Button
-						variant="outline-success"
+						variant='outline-success'
 						onClick={() => this.onClick()}
 						>
 							Login to Spotify
